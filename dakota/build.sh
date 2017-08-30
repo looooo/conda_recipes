4c95651fbde5f972808dbf09d44201c48af02959
looooo <sppedflyer@gmail.com>
2017-08-30 17:00:08 +0200

    dakota

--- dakota/build.sh
+++ dakota/build.sh
@@ -6,11 +6,17 @@ cd build
 cmake \
     -D CMAKE_BUILD_TYPE:STRING=RELEASE \
     -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+    -D DAKOTA_EXAMPLES_INSTALL:PATH=$PREFIX/share/dakota \
+    -D DAKOTA_TEST_INSTALL:PATH=$PREFIX/share/dakota \
+    -D DAKOTA_TOPFILES_INSTALL:PATH=$PREFIX/share/dakota \
+    -D DAKOTA_PYTHON=ON \
+    -D DAKOTA_PYTHON_NUMPY=ON \
     -D HAVE_X_GRAPHICS=OFF \
-    -D BUILD_IN_TRILINOS=OFF \
-    /projects/dakota_src/dakota
+    -D DAKOTA_HAVE_MPI=ON \
+    /projects/dakota
 
 make -j $CPU_COUNT
 make install
 
+mv $PREFIX/examples $PREFIX/share/dakota/hopspack
 
