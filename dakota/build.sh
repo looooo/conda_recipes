e0cd08f02d38ecd4dfd2f5dc08cfccd51a071998
looooo <sppedflyer@gmail.com>
2017-08-30 10:09:58 +0200

    dakota + pytrilinos

---
+++ dakota/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p build
+cd build
+
+cmake \
+    -D CMAKE_BUILD_TYPE:STRING=RELEASE \
+    -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
+    -D HAVE_X_GRAPHICS=OFF \
+    -D BUILD_IN_TRILINOS=OFF \
+    /projects/dakota_src/dakota
+
+make -j $CPU_COUNT
+make install
+
+
