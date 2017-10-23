3be37a6b9c45aca9d10f15309ac063d04bed8d45
looooo <sppedflyer@gmail.com>
2017-10-23 12:46:17 +0200

    dakota mpi

--- dakota/build.sh
+++ dakota/build.sh
@@ -7,7 +7,8 @@ cmake \
     -D CMAKE_BUILD_TYPE:STRING=RELEASE \
     -D CMAKE_INSTALL_PREFIX:PATH=$PREFIX \
     -D HAVE_X_GRAPHICS=OFF \
-    -D BUILD_IN_TRILINOS=OFF \
+    -D DAKOTA_HAVE_MPI=ON \
+    -D DAKOTA_PYTHON=ON \
     /projects/dakota
 
 make -j $CPU_COUNT
